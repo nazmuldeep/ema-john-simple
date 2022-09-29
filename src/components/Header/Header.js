d8f83ce6ba993b1036f574b9e287588d1cddbb04
nazmuldip <nazmuldip21@gmail.com>
2022-09-29 19:44:57 +0600

    data load

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/order">Orders</a>
+                <a href="/inventory">Inventroy</a>
+                <a href="/about">About</a>
+            </div>
+
+        </nav>
+    );
+};
+
+export default Header;
